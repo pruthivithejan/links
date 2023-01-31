8aa9ca6a805e27db547d8001b6b821bc5ef23d4d
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2023-01-31 17:32:23 +0530

    Next.js 13 Linktree

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,31 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import {Cards, githubUsername, codeRepository, slogan} from './content'
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          "{slogan.words}&nbsp;
+          <code className={styles.code}>{slogan.hightlight}</code>."
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href={`https://github.com/pruthivithejan/${codeRepository}`}
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            Code available in{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/github.svg"
+              alt="Github Logo"
               className={styles.vercelLogo}
-              width={100}
+              width={24}
               height={24}
               priority
             />
@@ -32,60 +34,44 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
         <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          <Image 
+           src={`https://avatars.githubusercontent.com/${githubUsername}`}
+           alt="Avatar" 
+           width={280}
+           height={280}
+           />
         </div>
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+         {
+          Cards.map((card, index) => {
+            return(
+              <a
+                href={card.href}
+                 className={styles.card}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={index}
+              > 
+                <h2 className={inter.className}>
+                  <Image
+                  src={`/${card.icon}.svg`}
+                  alt="Icon"
+                  className={styles.vercelLogo}
+                  width={24}
+                  height={24}
+                  />
+                  {card.title} <span>-&gt;</span>
+                </h2>
+                <p className={inter.className}>
+                 {card.description}
+                </p>
+              </a> 
+            )})
+          }
+       </div>
+     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
