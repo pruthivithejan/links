670409958b0fcbb3d32f32d95cee37a5265255ce
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2023-02-06 20:20:05 +0530

    added License

--- app/content.ts
+++ app/content.ts
@@ -1,52 +1,58 @@
-export const codeRepository = "linktree"
-export const githubUsername = "pruthivithejan"
-export const heading = "Pruthivi Thejan | Linktree"
+export const codeRepository = "linktree";
+export const githubUsername = "pruthivithejan";
+export const heading = "Pruthivi Thejan | Linktree";
 export const slogan = {
-    words: "I'll be myself or",
-    hightlight: "die trying",
-}     
-        
+  words: "I'll be myself or",
+  hightlight: "die trying",
+};
+
 export const Cards = [
-    {
-        href: "https://mobile.twitter.com/pruthivithejan",
-        icon: 'twitter',
-        title: 'Twitter' ,
-        description : 'I care about Twitter, I hope Elon Musk will not ruin it.',
-    },
-    {
-        href: "https://www.instagram.com/pruthivithejan/",
-        icon: 'instagram',
-        title: 'Instagram' ,
-        description : 'When I take good photos, I post it here.',
-    },
-    {
-        href: "https://www.reddit.com/user/pruthivithejan/",
-        icon: 'reddit',
-        title: 'Reddit' ,
-        description : 'For most Q&A, I use reddit.',
-    },
-    {
-        href: "https://t.me/pruthivithejan/",
-        icon: 'telegram',
-        title: 'Telegram' ,
-        description : 'Telegram is my most used messaging app.',
-    },
-    {
-        href: "https://link.friendspire.com/2TCHVXcy8ub",
-        icon: 'friendspire',
-        title: 'Friendspire' ,
-        description : 'Movies, TV Shows, Books and Podcasts are rated here.',
-    },   
-    {
-        href: "https://open.spotify.com/user/cipqdclr942xwcn7tdeo14o06?si=14e86fca3b8e462d",
-        icon: 'spotify',
-        title: 'Spotify' ,
-        description : 'My Spotify playlists are here.'
-    },   
-    {
-        href: "https://github.com/pruthivithejan",
-        icon: 'github',
-        title: 'Github' ,
-        description : 'I upload all my codes here.',
-    },   
-]
+  {
+    href: "https://mobile.twitter.com/pruthivithejan",
+    icon: "twitter",
+    title: "Twitter",
+    description: "I care about Twitter, I hope Elon Musk will not ruin it.",
+  },
+  {
+    href: "https://www.instagram.com/pruthivithejan/",
+    icon: "instagram",
+    title: "Instagram",
+    description: "When I take good photos, I post it here.",
+  },
+  {
+    href: "https://www.reddit.com/user/pruthivithejan/",
+    icon: "reddit",
+    title: "Reddit",
+    description: "For most Q&A, I use reddit.",
+  },
+  {
+    href: "https://t.me/pruthivithejan/",
+    icon: "telegram",
+    title: "Telegram",
+    description: "Telegram is my most used messaging app.",
+  },
+  {
+    href: "https://link.friendspire.com/2TCHVXcy8ub",
+    icon: "friendspire",
+    title: "Friendspire",
+    description: "Movies, TV Shows, Books and Podcasts are rated here.",
+  },
+  {
+    href: "https://open.spotify.com/user/cipqdclr942xwcn7tdeo14o06?si=14e86fca3b8e462d",
+    icon: "spotify",
+    title: "Spotify",
+    description: "My Spotify playlists are here.",
+  },
+  {
+    href: "https://github.com/pruthivithejan",
+    icon: "github",
+    title: "Github",
+    description: "I upload all my codes here.",
+  },
+  {
+    href: "https://facebook.com/mdpthejan",
+    icon: "facebook",
+    title: "Facebook",
+    description: "I don't use facebook much.",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-import {Cards, githubUsername, codeRepository, slogan} from './content'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import { Cards, githubUsername, codeRepository, slogan } from "./content";
 
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -20,7 +19,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Code available in{' '}
+            Made by Pruthivi Thejan{" "}
             <Image
               src="/github.svg"
               alt="Github Logo"
@@ -35,43 +34,40 @@ export default function Home() {
 
       <div className={styles.center}>
         <div className={styles.thirteen}>
-          <Image 
-           src={`https://avatars.githubusercontent.com/${githubUsername}`}
-           alt="Avatar" 
-           width={280}
-           height={280}
-           />
+          <Image
+            src={`https://avatars.githubusercontent.com/${githubUsername}`}
+            alt="Avatar"
+            width={280}
+            height={280}
+          />
         </div>
       </div>
 
       <div className={styles.grid}>
-         {
-          Cards.map((card, index) => {
-            return(
-              <a
-                href={card.href}
-                 className={styles.card}
-                target="_blank"
-                rel="noopener noreferrer"
-                key={index}
-              > 
-                <h2 className={inter.className}>
-                  <Image
+        {Cards.map((card, index) => {
+          return (
+            <a
+              href={card.href}
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={index}
+            >
+              <h2 className={inter.className}>
+                <Image
                   src={`/${card.icon}.svg`}
                   alt="Icon"
                   className={styles.vercelLogo}
                   width={24}
                   height={24}
-                  />
-                  {card.title} <span>-&gt;</span>
-                </h2>
-                <p className={inter.className}>
-                 {card.description}
-                </p>
-              </a> 
-            )})
-          }
-       </div>
-     </main>
-  )
+                />
+                {card.title} <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>{card.description}</p>
+            </a>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
